e740496eca1eb2c2ab9ab7b43ccae38ed905e4a6
Les Ansley <lesansley@Less-MacBook-Pro.local>
2020-04-21 21:35:05 +0200

    add just giving link

--- src/pages/giving.js
+++ src/pages/giving.js
@@ -6,6 +6,8 @@ import SEO from '../components/seo'
 import givingStyles from './giving.module.scss'
 
 const Giving = () => {
+	const widget = `<a href="//widgets.justgiving.com/Button/Redirect?p=eyJJZCI6ImE3MDg4N2JlLTk2YTctNDExMi1hYmMwLWNkMTI1ZTJlNzMzMyIsIkNoYXJpdHlJZCI6MTQzMTAzLCJTaXplIjoibCIsIlJlZmVyZW5jZSI6ImliaHUiLCJUeXBlIjoiRG9uYXRlIn0="><img src="//widgets.justgiving.com/Button?p=eyJJZCI6ImE3MDg4N2JlLTk2YTctNDExMi1hYmMwLWNkMTI1ZTJlNzMzMyIsIkNoYXJpdHlJZCI6MTQzMTAzLCJTaXplIjoibCIsIlJlZmVyZW5jZSI6ImliaHUiLCJUeXBlIjoiRG9uYXRlIn0=" /></a>`
+
 	return (
 		<Layout>
 			<SEO title='Giving Back page' />
@@ -21,6 +23,11 @@ const Giving = () => {
 					We felt that it was important that the brand is underpinned by a strong conservation message.
 					So 15% of the profits from the sale of Indlovu gin will be donated to the Africa Foundation to
 					support their work in wildlife conservation at Phinda Game Reserve.
+					<div className={givingStyles.donateButton}>
+						<a href="//widgets.justgiving.com/Button/Redirect?p=eyJJZCI6ImE3MDg4N2JlLTk2YTctNDExMi1hYmMwLWNkMTI1ZTJlNzMzMyIsIkNoYXJpdHlJZCI6MTQzMTAzLCJTaXplIjoibCIsIlJlZmVyZW5jZSI6ImliaHUiLCJUeXBlIjoiRG9uYXRlIn0=">
+							<img src="//widgets.justgiving.com/Button?p=eyJJZCI6ImE3MDg4N2JlLTk2YTctNDExMi1hYmMwLWNkMTI1ZTJlNzMzMyIsIkNoYXJpdHlJZCI6MTQzMTAzLCJTaXplIjoibCIsIlJlZmVyZW5jZSI6ImliaHUiLCJUeXBlIjoiRG9uYXRlIn0=" />
+						</a>
+					</div>
 				</div>
 				<div className={givingStyles.givingBathosInfo}>
 					Ibhu provides the cooking gas to Langrug Soup Kitchen at Batho’s Place, Franschhoek, which
