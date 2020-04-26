44b1fe068fe86ff814608806a0337ccaf11a0429
Les Ansley <lesansley@Less-MacBook-Pro.local>
2020-04-26 22:12:43 +0200

    Add rudimentary contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,19 +1,78 @@
-import React from "react"
-import { Link } from "gatsby"
+import React, { Component } from 'react'
+import Layout from '../components/layout'
+import SEO from '../components/seo';
+import contactLayout from './contact.module.scss'
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+class Contact extends Component {
+  constructor(props) {
+		super(props);
+		this.state = {
+			name: '',
+			email: '',
+			subject: '',
+			message: ''
+		}
+  }
 
-const Contact = () => {
-	
+render() {
 	return (
 		<Layout>
-		<SEO title="Contact" />
-		<h1>Hi from the Contact page</h1>
-		<p>Welcome to Contact page</p>
-		<Link to="/">Go back to the homepage</Link>
+			<SEO title='Contact' />
+			<div className={contactLayout.contain}>
+				<div className={contactLayout.wrapper}>
+					<form className={contactLayout.form} onSubmit={this.handleSubmit.bind(this)} method='POST'>
+					<p className={contactLayout.p}>
+						<label for="" className={contactLayout.label}>Your Name (Required)</label>
+							<input className={contactLayout.input} type="text" value={this.state.name} onChange={this.onNameChange.bind(this)} />
+					</p>
+					<p className={contactLayout.p}>
+						<label for="" className={contactLayout.label}>Your Email (Required)</label>
+						<input className={contactLayout.input} type="text" value={this.state.email} onChange={this.onEmailChange.bind(this)} />
+					</p>
+					<p className={contactLayout.p}>
+						<label for="" className={contactLayout.label}>Subject</label>
+						<input className={contactLayout.input} type="text" value={this.state.subject} onChange={this.onSubjectChange.bind(this)} />
+					</p>
+					<p  className={contactLayout.p}>
+						<label for="" className={contactLayout.label}>Your Message</label>
+						<textarea className={contactLayout.textArea} name="" id="" cols="30" rows='7' value={this.state.message} onChange={this.onMessageChange.bind(this)} ></textarea>
+					</p>
+					<p>
+						<button className={contactLayout.button}>Send</button>
+					</p>
+					</form>
+					<div className={contactLayout.details}>
+						<div>Our contact details</div>
+						<ul>
+							<li>San Joe St.</li>
+							<li>00-1212121-11</li>
+							<li>mail@mail.com</li>
+						</ul>
+					</div>
+				</div>
+			</div>	
 		</Layout>
-	)
+	);
 }
 
-export default Contact
+  onNameChange(event) {
+	this.setState({name: event.target.value})
+  }
+
+  onEmailChange(event) {
+	this.setState({email: event.target.value})
+	}
+	
+	onSubjectChange(event) {
+		this.setState({subject: event.target.value})
+		}
+
+  onMessageChange(event) {
+	this.setState({message: event.target.value})
+  }
+
+handleSubmit(event) {
+}
+}
+
+export default Contact;
