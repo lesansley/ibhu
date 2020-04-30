4e373f6961f592d81c3cf807fc55644ba3a5c989
Les Ansley <lesansley@Less-MacBook-Pro.local>
2020-04-30 10:00:08 +0200

    Add submission acknowledgement to contact page

---
+++ src/components/contact-submit.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import contactSubmitStyles from './contact-submit.module.scss';
+
+const ContactSubmit = () => {
+	return (
+		<div className={contactSubmitStyles.container}>
+			Thanks for getting in touch with us. We will respond to your message soon as possible	.
+		</div>
+	)
+}
+
+export default ContactSubmit
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,8 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { StaticQuery, graphql } from 'gatsby';
 import Layout from '../components/layout'
 import SEO from '../components/seo';
-import contactLayout from './contact.module.scss'
-
+import ContactForm from '../components/contact-form';
+import Divider from '../components/divider';
+import ContactBar from '../components/contact-bar';
+import ContactSubmit from '../components/contact-submit';
+//import contactLayout from './contact.module.scss'
 class Contact extends Component {
   constructor(props) {
 		super(props);
@@ -10,69 +14,61 @@ class Contact extends Component {
 			name: '',
 			email: '',
 			subject: '',
-			message: ''
+			message: '',
+			submitted: false
 		}
   }
 
-render() {
-	return (
-		<Layout>
-			<SEO title='Contact' />
-			<div className={contactLayout.contain}>
-				<div className={contactLayout.wrapper}>
-					<form className={contactLayout.form} onSubmit={this.handleSubmit.bind(this)} method='POST'>
-					<p className={contactLayout.p}>
-						<label for="" className={contactLayout.label}>Your Name (Required)</label>
-							<input className={contactLayout.input} type="text" value={this.state.name} onChange={this.onNameChange.bind(this)} />
-					</p>
-					<p className={contactLayout.p}>
-						<label for="" className={contactLayout.label}>Your Email (Required)</label>
-						<input className={contactLayout.input} type="text" value={this.state.email} onChange={this.onEmailChange.bind(this)} />
-					</p>
-					<p className={contactLayout.p}>
-						<label for="" className={contactLayout.label}>Subject</label>
-						<input className={contactLayout.input} type="text" value={this.state.subject} onChange={this.onSubjectChange.bind(this)} />
-					</p>
-					<p  className={contactLayout.p}>
-						<label for="" className={contactLayout.label}>Your Message</label>
-						<textarea className={contactLayout.textArea} name="" id="" cols="30" rows='7' value={this.state.message} onChange={this.onMessageChange.bind(this)} ></textarea>
-					</p>
-					<p>
-						<button className={contactLayout.button}>Send</button>
-					</p>
-					</form>
-					<div className={contactLayout.details}>
-						<div>Our contact details</div>
-						<ul>
-							<li>San Joe St.</li>
-							<li>00-1212121-11</li>
-							<li>mail@mail.com</li>
-						</ul>
-					</div>
-				</div>
-			</div>	
-		</Layout>
-	);
-}
-
-  onNameChange(event) {
-	this.setState({name: event.target.value})
-  }
-
-  onEmailChange(event) {
-	this.setState({email: event.target.value})
+	formSubmit(data) {
+		this.setState(state => ({ data }));
+		this.setState({submitted: true});
 	}
-	
-	onSubjectChange(event) {
-		this.setState({subject: event.target.value})
+
+	pageDisplay() {
+
+	}
+
+	render() {
+		const submitted = this.state.submitted;
+		let display;
+		if (!submitted) {
+			display = <ContactForm formSubmit={this.formSubmit.bind(this)} />
+		} else {
+			display = <ContactSubmit />
 		}
 
-  onMessageChange(event) {
-	this.setState({message: event.target.value})
-  }
+		return (
+			<StaticQuery
+				query={(graphql`
+					query {
+						site(siteMetadata: {}) {
+							siteMetadata {
+								contact
+								email
+								name
+								tel
+								social {
+									handle
+									name
+									url
+								}
+							}
+						}
+					}
+				`)}
 
-handleSubmit(event) {
-}
+				render={data => (
+					<Layout>
+						<SEO title='Contact' />
+						<h1>Contact us</h1>
+						{display}
+						<Divider />
+						<ContactBar />
+					</Layout>
+				)}
+			/>
+		);
+	}
 }
 
 export default Contact;
