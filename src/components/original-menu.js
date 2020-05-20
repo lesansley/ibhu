30b4458d08a71524017a17921a330153d6bcb6d9
Les Ansley <lesansley@Less-MacBook-Pro.local>
2020-05-20 17:29:42 +0200

    Add menu to Original page

--- src/components/original-menu.js
+++ src/components/original-menu.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import originalMenuStyles from '../components/original-menu.module.scss';
 
 const OriginalMenu = () => {
 	return (
-		<div>
-			<ul>
-				<li>original</li>
-				<li>tasting notes</li>
-				<li>faq</li>
+		<div className={originalMenuStyles.container}>
+			<ul className={originalMenuStyles.list}>
+				<li className={`${originalMenuStyles.listItem} ${originalMenuStyles.large}`}><a href='#original'>Original</a></li>
+				<li className={`${originalMenuStyles.listItem} ${originalMenuStyles.small}`}><a href='#original'>Home</a></li>
+				<li className={originalMenuStyles.listItem}><a href='#provenance'>Provenance</a></li>
+				<li className={`${originalMenuStyles.listItem} ${originalMenuStyles.large}`}><a href='#tasting-notes'>Tasting notes</a></li>
+				<li className={`${originalMenuStyles.listItem} ${originalMenuStyles.small}`}><a href='#tasting-notes'>Notes</a></li>
+				<li className={`${originalMenuStyles.listItem} ${originalMenuStyles.large}`}><a href='#recommendations'>Recommendations</a></li>
+				<li className={`${originalMenuStyles.listItem} ${originalMenuStyles.large}`}><a href='#testamonials'>Testamonials</a></li>
+				<li className={originalMenuStyles.listItem}><a href='#faq'>Faq</a></li>
 			</ul>
 		</div>
 	)
