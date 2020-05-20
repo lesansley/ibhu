0752cd292c8db0956296cb845d64310b679dc5d4
Les Ansley <lesansley@Less-MacBook-Pro.local>
2020-05-20 17:33:10 +0200

    Update Original page with new components

--- src/pages/indlovu/original.js
+++ src/pages/indlovu/original.js
@@ -1,26 +1,59 @@
 import React from 'react';
-import { Link } from 'gatsby';
 import Layout from '../../components/layout';
 import SEO from '../../components/seo';
 import OriginalMenu from '../../components/original-menu';
-
+import FAQ from '../../components/faq';
+import Divider from '../../components/divider';
+import Recommendations from '../../components/recommendations';
+import TastingNotes from '../../components/tasting-notes';
+import Testamonials from '../../components/testamonials';
+import Provenance from '../../components/provenance';
+import OriginalIntro from '../../components/original-intro';
 import originalStyles from './original.module.scss';
+
 
 const Original = () => {
 	return (
 		<Layout>
 			<SEO title='Original Gin page' />
-			<OriginalMenu />
-			<h1 className={originalStyles.heading}>Hi from the Original page</h1>
-			<p>Welcome to Original page</p>
-			<Link to='/'>Go back to the homepage</Link>
+			<div className={originalStyles.menuContainer} >
+				<div className={originalStyles.menu}>
+					<OriginalMenu />
+				</div>
+			</div>
+			<div id='original' className={originalStyles.hyperJump}>
+				<h1 className={`${originalStyles.h1} all-caps`}>Indlovu Original <span className={originalStyles.large}>Gin</span></h1>
+				<OriginalIntro />
+			</div>
+			<Divider />
+			<div id='provenance' className={originalStyles.hyperJump}>
+				<h2   className='all-caps'>Provenance</h2>
+				<Provenance />
+			</div>
+			<Divider />	
+			<div id='tasting-notes' className={originalStyles.hyperJump}>
+				<h2 className='all-caps'>Tasting Notes</h2>
+				<TastingNotes gin='original' />
+			</div>
+			<Divider />
+			<div id='recommendations' className={originalStyles.hyperJump}>
+				<h2  className='all-caps'>Recommendations</h2>
+				<Recommendations gin='original' />
+			</div>
+			<Divider />
+			<div id='testamonials' className={originalStyles.hyperJump}>
+				<h2  className='all-caps'>Testamonials</h2>
+				<Testamonials />
+			</div>
+			<Divider />
+			<div id='faq' className={originalStyles.hyperJump}>
+				<h2  className='all-caps'>
+					<span className={originalStyles.small}>FAQs</span>
+					<span className={originalStyles.large}>Frequently Asked Questions</span></h2>
+				<FAQ />
+			</div>
 		</Layout>
 	)
 }
 
 export default Original
-/**
-INDLOVU GIN
-An innovative premium gin for the adventurous spirit and the discerning gin collector.
-The Ibhu brand is founded in the spirit of innovation, and Indlovu is our gin under that brand. Indlovu gin is made using multiple flavouring runs.
- */
